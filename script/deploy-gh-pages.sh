938b890e785c88eacded69eeb5e31bfb568c8b9c
Platane <me@platane.me>
2016-04-10 13:41:35 +0200

    fix scripts

--- script/deploy-gh-pages.sh
+++ script/deploy-gh-pages.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # build demo
-- rm -r -f ./dist; webpack
+rm -r -f ./dist; webpack
 
 # deploy demo to gh pages
-- cd ./dist
-- git init
-- git config user.name "Travis CI"
-- git config user.email "me@platane.me"
-- git add .
-- git commit -m "Deploy to GitHub Pages"
-- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-- cd ..
+cd ./dist
+git init
+git config user.name "Travis CI"
+git config user.email "me@platane.me"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+cd ..
